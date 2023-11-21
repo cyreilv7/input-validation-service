d26c9ee069ebddb8e945ed9685520abf743dd043
cyreilv7 <shoiiru3@gmail.com>
2023-11-20 19:54:38 -0800

    add examples of microservice usage

---
+++ examples/example.js
@@ -0,0 +1,20 @@
+function processData(data) {
+    // insert whatever logic for processing data
+    console.log(data);
+}
+
+async function validateZipcode() {
+    const url = `http://localhost:${3000 || process.env.PORT}/api/validateInput?zipcode=01851`;
+    const options = {
+        method: 'GET',
+    }
+    return await fetch(url, options)
+    .then(res => res.json())
+    .then(data => data)
+    .catch(err => {
+        throw Error(err.message);
+    })
+}
+
+const zipcode = await validateZipcode('01581');
+processData(zipcode);
